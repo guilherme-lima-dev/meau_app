b411e220279bdef235ff7cdff43f718673e24b90
João Pedro Sadéri <jpsaderi@hotmail.com>
2023-01-17 20:33:56 -0300

    Fix: Mudança Firebase

--- lib/info/endpoints.dart
+++ lib/info/endpoints.dart
@@ -1,4 +1,6 @@
-class Endpoints{
-  static const urlAuth = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyARAxArEVYHnNtXWCgxDPXUbMUbNu6TYb0";
-  static const urlRegister = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyARAxArEVYHnNtXWCgxDPXUbMUbNu6TYb0";
+class Endpoints {
+  static const urlAuth =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAZgfhDKG7MwqCKnWunYFe9WiXdr6i-W_E";
+  static const urlRegister =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAZgfhDKG7MwqCKnWunYFe9WiXdr6i-W_E";
 }
