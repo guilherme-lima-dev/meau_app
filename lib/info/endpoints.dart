30304ec0219fdd115db540323aed3c9b5af27134
João Pedro Sadéri <jpsaderi@hotmail.com>
2023-01-25 09:13:11 -0300

    Fix: permitir trocar de banco de dados

--- lib/info/endpoints.dart
+++ lib/info/endpoints.dart
@@ -1,5 +1,8 @@
 class Endpoints {
-  static const key = "AIzaSyAGdwCd272mx-7uHVs7CLhrb3qGRQJ-AFw";
+  static const key = "AIzaSyAGdwCd272mx-7uHVs7CLhrb3qGRQJ-AFw"; // meau-8d974
+  // static const key = "AIzaSyCwlIf_shZlYIDlf8x3ooW-Y-29S0TslHI"; // meau-6c661
+  // static const key = "AIzaSyAZgfhDKG7MwqCKnWunYFe9WiXdr6i-W_E"; // meau-472b4
+
   static const urlAuth =
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}";
   static const urlRegister =
--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -115,8 +115,18 @@ class NotificationService {
   sendNotification(
       String title, String message, String tokenNotification) async {
     //Esse token fica localizado em: config do projeto -> cloud messaging -> chave do servidor
+
+    // meau-8d974
     String token =
         "AAAA5ibyiPw:APA91bFh1yABxQdHATPDqJiikgVFJ4q-Fr3vPoj4vyLJufdkxc6JbZoC1JGNZ-Adp8ooS-ipy6efItxEl7MbzZd31hlv-imc6U0PV59AtwSttrUq2GWhs_YwHQDZ4rpvnkIligcIkuv2";
+
+    // // meau-6c661
+    // String token =
+    //     "AAAAX7EDEcM:APA91bE_KwHBPyOizo9C7YWIUY4XBoAFqZlbBEV-7K7yO3baRbNC7rmhmR2Lc6pcjlXt4dN_GrFdRvvgcPadUQCO2fszQfNKcmhoCFcnwUkk3zvJaM93aPTJsiVc2W8gk8rbMOVIP_AY";
+
+    // meau-472b4
+    // String token =
+    //     "AAAAgDdzT0I:APA91bF1eJdwLIBETqdB-6FT12Fv80uI5PthNxUvEk0Now7OZyWxaPhd9f2LEV2SGsfhT7yyEekKjDoERXmnCqVijK1hxQaAd7JdMP16I1QiO_JznftoJ2HZwgIhMOuIqMmykOEYbpFR";
 
     var res = await dioClient.post(
         "https://fcm.googleapis.com/fcm/send",
