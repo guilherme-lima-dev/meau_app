d5aedf13d1898cb51dbfa89992681602ce261cec
João Pedro Sadéri da Silva <jpsaderi@hotmail.com>
2023-02-02 11:47:59 -0300

    Fix: trocar database

--- lib/info/endpoints.dart
+++ lib/info/endpoints.dart
@@ -1,11 +1,10 @@
 class Endpoints {
   // static const key = "AIzaSyAGdwCd272mx-7uHVs7CLhrb3qGRQJ-AFw"; // meau-8d974
-  // static const key = "AIzaSyCwlIf_shZlYIDlf8x3ooW-Y-29S0TslHI"; // meau-6c661
-  static const key = "AIzaSyAkVSpch45fzhKjotWQfVawq9ngBuUnoxc"; // meau-gui
+  static const key = "AIzaSyCwlIf_shZlYIDlf8x3ooW-Y-29S0TslHI"; // meau-6c661
+  // static const key = "AIzaSyAkVSpch45fzhKjotWQfVawq9ngBuUnoxc"; // meau-gui
   // static const key = "AIzaSyAZgfhDKG7MwqCKnWunYFe9WiXdr6i-W_E"; // meau-472b4
 
   static const urlAuth =
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$key";
-  static const urlRegister =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$key";
+  static const urlRegister = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$key";
 }
