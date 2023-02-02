b3abd140b5dfde1b2f71db0a2a72eb89042d2626
João Pedro Sadéri <jpsaderi@hotmail.com>
2023-02-02 19:22:01 -0300

    Feat: menu inicial

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:meau/chat/homepage.dart';
 import 'package:meau/components/app_bar_component.dart';
 import 'package:meau/components/custom_drawer_menu.dart';
 import 'package:meau/controllers/user/auth_controller.dart';
+import 'package:meau/views/animals/list_all_animals_screen.dart';
+import 'package:meau/views/animals/list_interested_screen.dart';
+import 'package:meau/views/animals/register_animal_screen.dart';
 import 'package:meau/views/user/auth/login_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -45,12 +49,197 @@ class HomeScreen extends StatelessWidget {
         title: "MEAU",
         appBar: AppBar(),
       ),
-      body: Center(
-        child: Text(
-          "Bem vindo(a) \n ${authController.user.name}",
-          style: const TextStyle(fontSize: 40),
-        ),
-      ),
+      body: authController.authenticated
+          ? Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Bem vindo(a) \n ${authController.user.name}",
+                  style: const TextStyle(fontSize: 40),
+                  textAlign: TextAlign.center,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF5A900), borderRadius: BorderRadius.circular(20)),
+                      margin: const EdgeInsets.only(top: 15, bottom: 15),
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RegisterAnimalScreen()));
+                        },
+                        child: const Text(
+                          "Cadastrar animal",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF5A900), borderRadius: BorderRadius.circular(20)),
+                      margin: const EdgeInsets.only(top: 15, bottom: 15),
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ListAllAnimalsScreen()));
+                        },
+                        child: const Text(
+                          "Exibir animais dispoíveis",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF5A900), borderRadius: BorderRadius.circular(20)),
+                      margin: const EdgeInsets.only(top: 15, bottom: 15),
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const ListInterestedSreen()));
+                        },
+                        child: const Text(
+                          "Interessados em adotar",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF5A900), borderRadius: BorderRadius.circular(20)),
+                      margin: const EdgeInsets.only(top: 15, bottom: 15),
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context, MaterialPageRoute(builder: (context) => const MyHomePage()));
+                        },
+                        child: const Text(
+                          "Chat com interessados",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ))
+          : Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff909090), borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(top: 15, bottom: 15),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Você precisa estar logado para cadastrar um animal!'),
+                      ));
+                    },
+                    child: const Text(
+                      "Cadastrar animal",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff909090), borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(top: 15, bottom: 15),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Você precisa estar logado para ver os animais!'),
+                      ));
+                    },
+                    child: const Text(
+                      "Exibir animais dispoíveis",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff909090), borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(top: 15, bottom: 15),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Você precisa estar logado para ver os interessados!'),
+                      ));
+                    },
+                    child: const Text(
+                      "Interessados em adotar",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff909090), borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(top: 15, bottom: 15),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Você precisa estar logado para acessar o chat!'),
+                      ));
+                    },
+                    child: const Text(
+                      "Chat com interessados",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ),
+              ],
+            )),
     );
   }
 }
